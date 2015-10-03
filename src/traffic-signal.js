7a4cdea8690e27cd899007379cd090b473a2f7a9
Gregory D. Hopkins <greg.hopkins@gmail.com>
2015-10-03 03:38:32 -0400

    traggic signal can now play a sequence of frames

--- src/traffic-signal.js
+++ src/traffic-signal.js
@@ -1,11 +1,12 @@
+var _ = require('lodash');
 var b = require('bonescript');
 
 function TrafficSignal(signalToPinMapping) {
-  if (!signalToPinMapping) {
-    throw 'must provide a signal to pin mapping';
+  if (!_(signalToPinMapping).isPlainObject()) {
+    throw 'must provide a signal to pin mapping object';
   }
 
-  if (Object.getOwnPropertyNames(signalToPinMapping).length === 0) {
+  if (_(signalToPinMapping).isEmpty()) {
     throw 'signal to pin mapping must contain at least one mapping';
   }
 
@@ -13,26 +14,38 @@ function TrafficSignal(signalToPinMapping) {
 }
 
 TrafficSignal.prototype.init = function() {
-  for (var signal in this.signalToPinMapping) {
-    b.pinMode(this.signalToPinMapping[signal], b.OUTPUT);
-  }
+  _.forOwn(this.signalToPinMapping, function (pin, signal) {
+    b.pinMode(pin, b.OUTPUT);
+  });
 };
 
 TrafficSignal.prototype.set = function (signal, power) {
-  if (!signal) {
+  if (!_(signal).isString()) {
     throw 'must provide at least signal name';
   }
 
-  if (!this.signalToPinMapping.hasOwnProperty(signal)) {
+  if (!_(this.signalToPinMapping).has(signal)) {
     throw 'signal ' + signal + ' not available';
   }
 
-  if (typeof power === 'undefined') {
+  if (_(power).isUndefined()) {
     b.digitalWrite(this.signalToPinMapping[signal], b.HIGH);
   } else {
     b.digitalWrite(this.signalToPinMapping[signal], power ? b.HIGH : b.LOW);
   }
+};
 
+TrafficSignal.prototype.play = function (frames) {
+  if (!_.isArray(frames)) {
+    throw 'must provide an array of signal states';
+  }
+
+  var thiz = this; // TODO use lodash thisArg binding
+  _.forEach(frames, function (frame) {
+    _.forOwn(frame, function (power, signal) {
+      thiz.set(signal, power);
+    });
+  });
 };
 
 module.exports = {
